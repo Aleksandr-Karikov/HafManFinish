5eb26d8791e32b5527bd41cb4d28459bdc42f380
karikov Aleksandr <kaeikov.sanya2803@outlook.com>
2021-05-13 13:51:32 +0700

    Добавьте файлы проекта.

---
+++ Node.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HafManFinish
+{
+    class Node
+    {
+        private int frequence;//частота
+        private char letter;//буква
+        private Node leftChild;//левый потомок
+        private Node rightChild;//правый потомок
+
+        public Node(char letter, int frequence)
+        { //собственно, конструктор
+            this.letter = letter;
+            this.frequence = frequence;
+        }
+        public Node() { }
+
+        public void addChild(Node newNode)
+        {//добавить потомка
+            if (leftChild == null)//если левый пустой=> правый тоже=> добавляем в левый
+                leftChild = newNode;
+            else
+            {
+                if (leftChild.getFrequence() <= newNode.getFrequence()) //в общем, левым потомком
+                    rightChild = newNode;//станет тот, у кого меньше частота
+                else
+                {
+                    rightChild = leftChild;
+                    leftChild = newNode;
+                }
+            }
+
+            frequence += newNode.getFrequence();//итоговая частота
+        }
+        public Node getLeftChild()
+        {
+            return leftChild;
+        }
+
+        public Node getRightChild()
+        {
+            return rightChild;
+        }
+
+        public int getFrequence()
+        {
+            return frequence;
+        }
+
+        public char getLetter()
+        {
+            return letter;
+        }
+
+        public bool isLeaf()
+        {//проверка на лист
+            return leftChild == null && rightChild == null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.IO;
+namespace HafManFinish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding c = Encoding.ASCII;
+            string path = @"C:\Users\Александр\source\repos\HafManFinish\test.txt";
+            byte[] a = new byte[0];
+            a = c.GetBytes("k");
+            Console.WriteLine(Convert.ToString(a[0]));
+            using (FileStream fstream = new FileStream($"{path}", FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] array = System.Text.Encoding.Default.GetBytes("0101");
+                // запись массива байтов в файл
+                //string text = "AAA";
+              //  fstream.WriteByte(101);
+                byte[] t = System.Text.Encoding.Default.GetBytes("e");
+                Console.WriteLine(Convert.ToString(t[0], 2));
+
+                fstream.WriteByte(101);
+                Console.WriteLine("Текст записан в файл");
+            }
+        }
+    }
+}
